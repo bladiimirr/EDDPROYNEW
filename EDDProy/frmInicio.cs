60688b4489f06025776a74b0884823cab8a6e181
GL66 <GL66@MSI>
2024-11-11 19:57:48 -0800

    nuevo

--- EDDProy/frmInicio.cs
+++ EDDProy/frmInicio.cs
@@ -46,5 +46,10 @@ namespace EDDemo
             mArboles.MdiParent = this;
             mArboles.Show();
         }
+
+        private void estructurasNoLibealesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
